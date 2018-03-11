aaaf2928bdc1469c52ee49c8b94df35f9e2a169f
yugecin <yugecin@users.noreply.github.com>
2018-03-11 01:13:18 +0100

    add formatting tips in format editor

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -23,10 +23,12 @@
 		/// the contents of this method with the code editor.
 		/// </summary>
 		private void InitializeComponent() {
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
 			this.label = new System.Windows.Forms.Label();
 			this.cancel = new System.Windows.Forms.Button();
 			this.save = new System.Windows.Forms.Button();
 			this.textbox = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
 			this.SuspendLayout();
 			// 
 			// label
@@ -37,7 +39,7 @@
 			this.label.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
 			this.label.Location = new System.Drawing.Point(12, 9);
 			this.label.Name = "label";
-			this.label.Size = new System.Drawing.Size(560, 109);
+			this.label.Size = new System.Drawing.Size(560, 110);
 			this.label.TabIndex = 0;
 			this.label.Text = "label1";
 			// 
@@ -45,7 +47,7 @@
 			// 
 			this.cancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
 			this.cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-			this.cancel.Location = new System.Drawing.Point(416, 205);
+			this.cancel.Location = new System.Drawing.Point(416, 430);
 			this.cancel.Name = "cancel";
 			this.cancel.Size = new System.Drawing.Size(75, 23);
 			this.cancel.TabIndex = 1;
@@ -56,7 +58,7 @@
 			// save
 			// 
 			this.save.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-			this.save.Location = new System.Drawing.Point(497, 205);
+			this.save.Location = new System.Drawing.Point(497, 430);
 			this.save.Name = "save";
 			this.save.Size = new System.Drawing.Size(75, 23);
 			this.save.TabIndex = 2;
@@ -68,12 +70,24 @@
 			// 
 			this.textbox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-			this.textbox.Location = new System.Drawing.Point(12, 121);
+			this.textbox.Location = new System.Drawing.Point(16, 122);
 			this.textbox.Multiline = true;
 			this.textbox.Name = "textbox";
 			this.textbox.Size = new System.Drawing.Size(560, 78);
 			this.textbox.TabIndex = 3;
 			this.textbox.TextChanged += new System.EventHandler(this.textbox_TextChanged);
+			// 
+			// label1
+			// 
+			this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.label1.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label1.Location = new System.Drawing.Point(13, 203);
+			this.label1.Name = "label1";
+			this.label1.Padding = new System.Windows.Forms.Padding(5);
+			this.label1.Size = new System.Drawing.Size(563, 224);
+			this.label1.TabIndex = 4;
+			this.label1.Text = resources.GetString("label1.Text");
 			// 
 			// Form2
 			// 
@@ -81,7 +95,8 @@
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.CancelButton = this.cancel;
-			this.ClientSize = new System.Drawing.Size(584, 240);
+			this.ClientSize = new System.Drawing.Size(584, 465);
+			this.Controls.Add(this.label1);
 			this.Controls.Add(this.textbox);
 			this.Controls.Add(this.save);
 			this.Controls.Add(this.cancel);
@@ -99,5 +114,6 @@
 		private System.Windows.Forms.Button cancel;
 		private System.Windows.Forms.Button save;
 		private System.Windows.Forms.TextBox textbox;
+		private System.Windows.Forms.Label label1;
 	}
 }
